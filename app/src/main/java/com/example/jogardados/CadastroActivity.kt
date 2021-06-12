49c654af3c7ff1d0900d006da91ad2449da5ff43
gleidston <king2010-ro@hotmail.com>
2021-06-12 02:59:59 -0300

    MVP parte 3

--- app/src/main/java/com/example/jogardados/CadastroActivity.kt
+++ app/src/main/java/com/example/jogardados/CadastroActivity.kt
@@ -1,11 +1,26 @@
 package com.example.jogardados
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
-class Cadastro : AppCompatActivity() {
+
+class CadastroActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cadastro)
+        val jogador1 = findViewById<EditText>(R.id.txt_jogador)
+        val btnCadastro = findViewById<Button>(R.id.button_cadastro)
+
+        btnCadastro.setOnClickListener {
+            var jogador = jogador1.text.toString()
+            val intent = Intent(this, MainActivity::class.java)
+            intent.putExtra("jogador1", jogador)
+            startActivities(arrayOf(intent))
+
+        }
     }
+
 }
--- app/src/main/java/com/example/jogardados/MainActivity.kt
+++ app/src/main/java/com/example/jogardados/MainActivity.kt
@@ -1,33 +1,91 @@
 package com.example.jogardados
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("LongLogTag")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val dado1 = findViewById<TextView>(R.id.dado1)
-        val dado2 = findViewById<TextView>(R.id.dado2)
-        val bt6 =findViewById(R.id.btn_6)as Button
+        val dado1 = findViewById<ImageView>(R.id.imgDado1)
+        val dado2 = findViewById<ImageView>(R.id.imgDado2)
+        val bt6 = findViewById<Button>(R.id.btn_6)
+        val zap = findViewById<FloatingActionButton>(R.id.floatWhats)
+        val text = findViewById<TextView>(R.id.txtBoas)
+        val jogador = intent.getStringExtra("jogador1")
+
+        val msg = "${resources.getString(R.string.bemvindo)}$jogador"
+
+        text.text = msg
+
+
+        val imagens = listOf(
+            R.drawable.dice_1, R.drawable.dice_2, R.drawable.dice_3, R.drawable.dice_4,
+            R.drawable.dice_5, R.drawable.dice_6
+        )
 
         bt6.setOnClickListener {
 
-            dado1.text=gerardados().toString()
-            dado2.text=gerardados().toString()
+            dado1.setImageResource(imagens.random())
+            dado2.setImageResource(imagens.random())
 
 
         }
-        dado1.text=gerardados().toString()
-        dado2.text=gerardados().toString()
-    }
-    private fun gerardados (): Int {
-       // val rand = (1..6).random()
-        return (1..6).random()
+        zap.setOnClickListener {
+
+            val intent =Intent(Intent.ACTION_SEND)
+            intent.putExtra(Intent.EXTRA_TEXT,"Voce e sortudo ")
+            intent.setPackage("com.whatsapp")
+            intent.type="text/plain"
+            if (intent.resolveActivity(this.packageManager)!=null)
+                startActivities(arrayOf(intent))
+
+        }
+
+        /* ultilizei o log  para visualizar  em etapas o ciclo de vida da activity
+        *  atravez do Logcat durante o debug  do programa
+        * abaixo criei funçoes com  onCreate onStart onResume onPause e onDestroy
+        * documentaçao nesse link : https://developer.android.com/guide/components/activities/activity-lifecycle?hl=pt-br  */
+
+        Log.i("Ciclo de vida da activity  ", "on create")
+
     }
 
 
+
+    override fun onStart() {
+        super.onStart()
+        Log.i("ciclo de vida ", "onStart")
+    }
+
+
+    override fun onResume() {
+        super.onResume()
+        Log.i("ciclo de vida ", "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.i("ciclo de vida ", "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i("ciclo de vida ", "onstop ")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i("ciclo de vida ", "onDestroy")
+    }
 }
